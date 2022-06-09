783588e44a28de28a85e7771ec355ad3f077ae7d
Andrey Demyanov <demyanov23@gmail.com>
2022-06-09 11:23:40 +0300

    updating about field

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -7,8 +7,8 @@ const About: FC = () => {
 
       <p className='about__text'>I’m Andrey Demyanov — frontend developer with passion to React, UI and web design. 
       I like to create interfaces with modern stack without forgetting about performance and swiftness.</p>
+      <p className='about__text'>My main stack is React, Redux, TypeScript. I prefer Scss or MaterialUI / styled-components for styling. If you’re doing some useful and interesting project and think that I could help you — write me on email or at the telegram :)</p>
       <p className='about__text'>Out of laptop I like to birdwatching, travelling and play Catan.</p>
-      <p className='about__text'>If you want to contact me — write me on email or at the telegram :)</p>
     </section>
   )
 };
