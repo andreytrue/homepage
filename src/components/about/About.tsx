3d1d0fb5ec1948c7c07bf581e0fcafa3808324a0
Andrey Demyanov <demyanov23@gmail.com>
2023-08-13 10:34:02 +0300

    update stack info

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,16 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
 const About: FC = () => {
   return (
-    <section className='main__about about'>
-      <h1 className='about__header'>Hello &#128075;</h1>
+    <section className="main__about about">
+      <h1 className="about__header">Hello &#128075;</h1>
 
-      <p className='about__text'>I’m Andrey Demyanov — frontend developer with passion to React, UI and web design. 
-      I like to create interfaces with modern stack without forgetting about performance and swiftness.</p>
-      <p className='about__text'>My main stack is React, Redux, TypeScript. I prefer Scss or MaterialUI / styled-components for styling. If you’re doing some useful and interesting project and think that I could help you — write me on email or at the telegram :)</p>
-      <p className='about__text'>Out of laptop I like to birdwatching, travelling and play Catan.</p>
+      <p className="about__text">
+        I’m Andrey Demyanov — frontend developer with passion to React, UI and web design. I like to create interfaces
+        with modern stack without forgetting about performance and swiftness.
+      </p>
+      <p className="about__text">
+        My main stack is Next.js, React, TypeScript. I prefer Tailwind, but also can work with Scss or MaterialUI /
+        styled-components for styling. If you’re doing some useful and interesting project and think that I could help
+        you — write me on email or at the telegram :)
+      </p>
+      <p className="about__text">Out of laptop I like to birdwatching, travelling and play Catan.</p>
     </section>
-  )
+  );
 };
 
 export default About;
--- src/components/facts/Facts.tsx
+++ src/components/facts/Facts.tsx
@@ -1,16 +1,22 @@
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
 const Facts: FC = () => {
   return (
     <section className="main__facts facts">
-      <h3 className='facts__header'>Bonus facts about me:</h3>
+      <h3 className="facts__header">Bonus facts about me:</h3>
 
-      <ul className='facts__list'>
-        <li className='facts__item'>I’ve been a head of media & marketing in one of the best handball clubs in the world and won a prize for the best project for fans in Russia in all sports.</li>
-        <li className='facts__item'>I collect mosaic bus stops around the ex-soviet countries and creating a web site to store them. Text me ahywhere if you like them or saw some to add it on map!</li>
+      <ul className="facts__list">
+        <li className="facts__item">
+          I’ve been a head of media & marketing in one of the best handball clubs in the world and won a prize for the
+          best project for fans in all sports at that year.
+        </li>
+        <li className="facts__item">
+          I collect mosaic bus stops around the ex-soviet countries and creating a web site to store them. Text me
+          ahywhere if you like them or saw some to add it on map!
+        </li>
       </ul>
     </section>
-  )
+  );
 };
 
 export default Facts;
